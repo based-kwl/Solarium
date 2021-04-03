ce5223974821d91c5f0e26b6592bbd8ccb00a3a3
PhunnieB <phunniehypnotix@gmail.com>
2021-04-03 17:14:28 -0400

    Logger

--- Solarium/Engine/Solarium.cpp
+++ Solarium/Engine/Solarium.cpp
@@ -5,10 +5,11 @@
 #include "../Defines.h"
 #include "Solarium.h"
 #include <GLFW/glfw3.h>
-
+#include "Logger.h"
 using namespace std;
 
 int main( int argc, const char** argv )
 {
+	Solarium::Logger::Log("Lukas is dum and has %d chromies", 50);
 	return 0;
 }
