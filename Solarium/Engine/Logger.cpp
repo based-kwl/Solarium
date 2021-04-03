250094ae337635f2f7c8c758cca88ded49533e2a
PhunnieB <phunniehypnotix@gmail.com>
2021-04-03 16:31:22 -0400

    Basic Setup

---
+++ Solarium/Engine/Logger.cpp
@@ -0,0 +1,19 @@
+#include "Logger.h"
+
+namespace Solarium {
+	void Logger::Trace(const char* message, ...) {
+
+	}
+	void Logger::Log(const char* message, ...) {
+
+	}
+	void Logger::Warn(const char* message, ...) {
+
+	}
+	void Logger::Error(const char* message, ...) {
+
+	}
+	void Logger::Fatal(const char* message, ...) {
+
+	}
+}
---
+++ Solarium/Engine/Solarium.cpp
@@ -0,0 +1,15 @@
+﻿// Solarium.cpp : Defines the entry point for the application.
+//
+
+#include "../typedef.h"
+#include "../defines.h"
+#include "Solarium.h"
+#include <stdint.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+int main( int argc, const char** argv )
+{
+	return 0;
+}
