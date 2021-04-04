2cf45f3a0c47c25bdb756e2846823b9e85a151e7
LucasAstro <airmasterjohn@outlook.com>
2021-04-03 20:56:13 -0500

    Commit

---
+++ Solarium/Engine/VulkanRenderer.cpp
@@ -0,0 +1,17 @@
+#include "VulkanRenderer.hpp"
+#include "VulkanHelper.hpp"
+
+namespace Solarium
+{
+    VulkanRenderer::VulkanRenderer(Platform* platform)
+    {
+        GLFWwindow* window = platform->GetWindow();
+        
+        
+    }
+	VulkanRenderer::~VulkanRenderer()
+    {
+
+    }
+
+}
