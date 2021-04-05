f8b9714059810a373d240d515a52ba975d49fd7f
PhunnieB <phunniehypnotix@gmail.com>
2021-04-05 17:34:09 -0400

    Revert "Merge pull request #8 from Zi7ar21/main"
    This reverts commit d32e83765b49b6b38ede38f493d89b3ab8626816, reversing
    changes made to e00738deda351a942920f2ae82b2150a3c887de8.

--- Solarium/Shaders/main.vert
+++ Solarium/Shaders/main.vert
@@ -2,9 +2,9 @@
 #extension GL_KHR_vulkan_glsl : enable
 vec2 positions[3] = vec2[]
 (
-	vec2(0.0, 0.0),
-	vec2(2.0, 0.0),
-	vec2(0.0, 2.0)
+	vec2(0.0, -0.5),
+	vec2(0.2, 0.5),
+	vec2(-0.5, 0.5)
 );
 
 void main()
--- Solarium/shadders/shadders/common.glsl
+++ Solarium/shadders/shadders/common.glsl
@@ -1,11 +1,7 @@
 // ##### CONSTANTS #####
-// http://www.mimirgames.com/articles/programming/digits-of-pi-needed-for-floating-point-numbers/
-// Full-Precision (32-Bit) Floating-Point Numbers can't represent Pi any further than ~3.14159265 but it's
-// good to the compiler give a few extra digits so it can choose the best Floating-Point Value possible
-#define pi 3.1415926535897932384626433832795028841971693993751058209749445923078164
+#define pi 3.14159265
 #define twopi pi*2.0
 
-// ##### NOISE #####
 // Cyclic Noise (2D)
 float cyclic(vec2 coord){
     float component1 = cos(coord.x);
