2ae0709d4a97abb706913f1cd9deef0359e09a11
Jacob Bingham <41303821+Zi7ar21@users.noreply.github.com>
2021-04-05 12:48:16 -0600

    Add Changes to Unused Shadders
    Don't hurt me

--- Solarium/shadders/shadders-rasterization/parameters.glsl
+++ Solarium/shadders/shadders-rasterization/parameters.glsl
@@ -1,9 +1,1 @@
-// ##### CONSTANTS #####
-// http://www.mimirgames.com/articles/programming/digits-of-pi-needed-for-floating-point-numbers/
-// Full-Precision (32-Bit) Floating-Point Numbers can't represent Pi any further than ~3.14159265 but it's
-// good to the compiler give a few extra digits so it can choose the best Floating-Point Value possible
-#define pi 3.1415926535897932384626433832795028841971693993751058209749445923078164
-#define twopi pi*2.0
-
-// ##### PARAMETERS #####
 #define gamma 2.2
